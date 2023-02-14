46576dabfcfb2f524bc0adbfc2d72d591a6e6d0d
Pirpyn <payen.pierre@gmail.com>
2023-02-14 23:11:52 +0100

    #19 add symbol-file

---
+++ scripts/all_regex.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+usage()
+{
+	cat << EOF
+usage: $(basename $0) word1 ...
+
+help: Given a word, prints the regex to list all possible case to write the word letter after one another, starting from none.
+i.e for the word foo, regex will match "", f, fo, foo, which is $($0 foo)
+The reason is gdb accepts partial command when there's no ambiguity, so regex must handle all possible writing configuration
+
+example: For symbol-file command, sy is enough, and to get all possibilities, one can use
+\`$(basename $0) mbol-file\`, then append sy to the regex to get sy$($0 mbol-file)
+EOF
+}
+
+regex()
+{
+	# taken a word foo, prints (?:f(?:o(?:o)?)?)?
+	# recursive strategy
+	local word="${1}"
+	if [[ ${#word} -le 0 ]]; then
+		return
+	else
+		printf "(?:${word:0:1}"
+		regex "${word:1}"
+		printf ")?"
+	fi
+}
+
+if [[ $# -eq 0 ]]; then
+	usage
+	exit 1
+fi
+
+for word in "${@}"; do
+	regex "${word}"
+	echo
+done
---
+++ tests/command/symbol-file.gdb
@@ -0,0 +1,14 @@
+sy
+sym
+symb
+symbo
+symbol
+symbol-
+symbol-f
+symbol-fi
+symbol-fil
+symbol-file
+symbol-file foo
+symbol-file foo -o 0x123
+symbol-file -readnow foo
+symbol-file -readnever foo
